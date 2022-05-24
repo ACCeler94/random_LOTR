872d65a016941cd5c1d604202d922091d4f37fea
Artur C <araghast94@icloud.com>
2022-05-24 22:01:28 +0200

    added human and dwarf names

---
+++ script.js
@@ -0,0 +1,19 @@
+const races = ['Dwarf', 'Human', 'Elf', 'Hobbit', 'Orc']
+
+const dwarfNames = ['Anar', 'Balin', 'Beli', 'Bifur', 'Bláin', 'Bofur', 'Bombur', 'Borin',
+   ' Burin', 'Dáin', 'Dori', 'Durin', 'Dwalin', 'Farin', 'Fíli', 'Flói', 'Frár',
+    'Frerin', 'Frór', 'Fundin', 'Gimli', 'Ginar', 'Glóin', 'Gróin', 'Grór',
+    'Hanar', 'Kíli',' Lófar', 'Lóni', 'Náin', 'Náli', 'Nár', 'Nói', 'Nori', 'Óin',
+    'Ónar', 'Óri', 'Póri', 'Regin', 'Torin', 'Tráin', 'Trór', 'Vid']
+
+const humanNamesMale = ['Bhân-goru-Bhân', 'Bhôn-rema-Bhôn',' Bhûn-daro-Bhûn', 'Dhân-moru-Dhân', 'Dhôn-bari-Dhôn', 'Dhûn-garo-Dhûn','Ghân-buri-ghân', 'Ghôn-rumi-ghôn', 
+    'Rhôn-damu-Rhôn', 'Rhûn-bamo-Rhûn, Aldor', 'Baldor', 'Baldred', 'Bregdan', 'Brego', 'Brytta', 'Ceorl', 'Déor', 'Déorbrand', 
+    'Déorgar', 'Déorhelm', 'Déorthain', 'Déorwine', 'Dernfara', 'Derngar', 'Dernhelm', 'Dernwine', 'Dúnhere', 'Elfhelm', 'Elfwine', 'Éadig', 'Éadmód', 'Éoblod', 'Éogar', 'Éohere', 'Éoheort', 'Éomer', 'Éomód', 'Éomund', 'Éorl', 'Éoman', 'Éorcanstan',
+    'Éored', 'Éothain' , 'Fram', 'Fréa', 'Fréaláf', 'Fréawine', 'Freca', 'Frumgar', 'Fulgar', 'Fulgrim', 'Fulor', 'Fulthain', 'Gálmód', 'Gamling', 'Gárbald', 'Gárulf', 'Gárwine', 'Gléobeam', 'Gléomer', 'Gléothain', 'Gléowine',
+    'Adrahil', 'Amlaith', 'Anardil', 'Anárion', 'Anborn', 'Angbor', 'Arador',' Araglas' , 'Aragorn', 'Aragost', 'Arahad', 'Arahael', 'Aranarth', 'Arannel', 'Aranuir',
+    'Araphant',' Araphor', 'Arassuil', 'Arathorn', 'Araval', 'Aravir', 'Aravorn', 'Argeleb', 'Argonui', 'Arvedui', 'Arvegil', 'Arveleg', 'Baranor', 'Belecthor', 
+    'Beleg', 'Belegorn', 'Beregond', 'Beren', 'Bergil', 'Boromir', 'Celepharn', 'Cirion', 'Damrod', 'Denethor', 'Derufin', 'Dervorin', 'Dírhael', 'Duilin']
+ 
+const humanNamesFemale = ['Déorhild', 'Déorwyn', 'Dernhild', 'Dernwyn', 'Elfhild', 'Elfwyn', 'Éohild', 'Éowyn', 'Gléowyn', 'Guthwyn' ,
+    'Holdwyn', 'Heruwyn', 'Hild', 'Léofwyn', 'Téodwyn', 'Gilraen', 'Ioreth', 'Ivorwen', 'Lothíriel', 'Morwen']
+
